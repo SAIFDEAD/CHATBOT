43d5bc2d48ce334822fa358ddf367f602b5de816
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-09 01:36:51 +0530

    Update eval.py

--- Venom/modules/eval.py
+++ Venom/modules/eval.py
@@ -126,7 +126,7 @@ async def forceclose_command(_, CallbackQuery):
     if CallbackQuery.from_user.id != int(user_id):
         try:
             return await CallbackQuery.answer(
-                "» ɪᴛ'ʟʟ ʙᴇ ʙᴇᴛᴛᴇʀ ɪғ ʏᴏᴜ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ʙᴀʙʏ.", show_alert=True
+                "» ɪᴛ'ʟʟ ʙᴇ ʙᴇᴛᴛᴇʀ ɪғ ʏᴏᴜ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ʙᴀʙʏ 👄", show_alert=True
             )
         except:
             return
