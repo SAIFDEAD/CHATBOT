136cbc387effdbd772d496f51e9b186b79bc246c
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-09 01:31:50 +0530

    Update ids.py

--- Venom/modules/ids.py
+++ Venom/modules/ids.py
@@ -11,8 +11,8 @@ async def getid(client, message):
     message_id = message.id
     reply = message.reply_to_message
 
-    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
-    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
+    text = f"**[𝐌ᴇssᴀɢᴇ 𝐈ᴅ:]({message.link})** `{message_id}`\n"
+    text += f"**[𝐘ᴏᴜʀ 𝐈ᴅ:](tg://user?id={your_id})** `{your_id}`\n"
 
     if not message.command:
         message.command = message.text.split()
@@ -24,20 +24,20 @@ async def getid(client, message):
         try:
             split = message.text.split(None, 1)[1].strip()
             user_id = (await client.get_users(split)).id
-            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user_id})** `{user_id}`\n"
+            text += f"**[𝐔sᴇʀ 𝐈ᴅ:](tg://user?id={user_id})** `{user_id}`\n"
 
         except Exception:
             return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
 
-    text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+    text += f"**[𝐂ʜᴀᴛ 𝐈ᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
 
     if (
         not getattr(reply, "empty", True)
         and not message.forward_from_chat
         and not reply.sender_chat
     ):
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+        text += f"**[𝐑ᴇᴘʟɪᴇᴅ 𝐌ᴇssᴀɢᴇ 𝐈ᴅ:]({reply.link})** `{reply.id}`\n"
+        text += f"**[𝐑ᴇᴘʟɪᴇᴅ 𝐔sᴇʀ 𝐈ᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
 
     if reply and reply.forward_from_chat:
         text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
